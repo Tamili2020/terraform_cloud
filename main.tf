2f460a67b4998f24e36554b81b3796c2f35a4a18
Tamili2020 <muthuppandiac@gmail.com>
2026-03-08 23:28:34 +0530

    Refactor S3 bucket resources to use a module
    Replaced existing S3 bucket resources with a module for better management.

--- main.tf
+++ main.tf
@@ -1,25 +1,5 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "firstbucket" {
-  bucket = "firstbucket1234876new"
-
-  tags = {
-    Name        = "firstbucket"
-    Environment = "Dev"
-    Created_by = "terraform_source"
-  }
-
-}
-
-resource "aws_s3_bucket" "firstbucket2" {
-  bucket = "muthufirs004"
-
-  tags = {
-    Name        = "firstbucket"
-    Environment = "Dev"
-    Created_by = "terraform_source"
-  }
-
+module "storage" {
+  source  = "app.terraform.io/Learning_self/storage/aws"
+  version = "0.0.1"
+  bucket_name = "my-bucket-1987muthu"
 }
